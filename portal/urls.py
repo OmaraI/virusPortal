2f6667405bad50d0ebf0e7bff1f81cad8490e7c3
AlfredUg <assekagiri@gmail.com>
2001-01-01 03:32:45 +0300

    update urls

--- portal/urls.py
+++ portal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('downloads/', views.downloads, name='downloads'),
     path('home/', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('summary/', views.summary, name='summary'),
--- portal/views.py
+++ portal/views.py
@@ -13,11 +13,39 @@ def  search(request):
 def  publications(request):
 	return render(request, 'publications.html')
 
-def  summary(request):
-	return render(request, 'summary.html')
-
 def  guidelines(request):
 	return render(request, 'guidelines.html')
 
 def  contact(request):
 	return render(request, 'contact.html')
+
+def  downloads(request):
+    samples=sample.objects.all()
+    viruses=virus.objects.all()
+    sites=site.objects.all()
+    projects=project.objects.all()
+    organisms=organism.objects.all()
+    return render(request, 'downloads.html', 
+							{'samples': samples, 
+              'viruses': viruses,
+              'sites': sites,
+              'projects': projects,
+              'organisms':organisms})
+
+def summary(request):
+    samples=sample.objects.all()
+    viruses=virus.objects.all()
+    sites=site.objects.all()
+    projects=project.objects.all()
+    organisms=organism.objects.all()
+
+    context={}
+    """ context={'xdata_assay': xdata_assay, 
+		'ydata_assay': ydata_assay,
+		'xdata_disease': xdata_disease, 
+		'ydata_disease': ydata_disease, 
+		'platform_pie_dict': platform_pie_dict, 
+		'site_pie_dict': site_pie_dict, 
+		'all_records': all_records};
+    """    
+    return render(request, 'summary.html', context=context)
